bca45f64dd81fae3bb63cd80cc33f7a8dff6f8d7
satyajit <satyajit_jbl@yahoo.com>
2024-09-18 23:51:20 +0600

    varify pin start

--- js/home.js
+++ js/home.js
@@ -1,1 +1,20 @@
-console.log('home page added');
+// add money to the account
+
+/*
+//step 1 : add an event handler to the add oney button inside the form
+prevent page reload after form submit
+s-2: get money to be added to the account balance
+*/
+document.getElementById('btn-add-money')
+    .addEventListener('click', function(event){
+        // prevent page reload after form submit
+    event.preventDefault();
+
+    //step 2: get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    // get pic number and verify it
+    const pinNumberInput = document.getElementById('input-pin-number');
+    console.log(pinNumberInput);
+});
